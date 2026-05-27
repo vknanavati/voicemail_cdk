0cf179f76088a75cb7738f8f27ea3e0d082c1edf
vknanavati <vimin.nanavati@gmail.com>
2026-05-27 09:56:01 -0400

    Initial commit

---
+++ test/master-demo-voicemail-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MasterDemoVoicemailCdk from '../lib/master-demo-voicemail-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/master-demo-voicemail-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MasterDemoVoicemailCdk.MasterDemoVoicemailCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
